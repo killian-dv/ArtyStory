f824b37fee15d734106d7514159021bf80acaa77
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-18 15:44:30 +0200

    ajustement du  prompt

--- src/views/MarieAntoinetteView.vue
+++ src/views/MarieAntoinetteView.vue
@@ -5,7 +5,7 @@ import Chatbot from '../components/Chatbot.vue';
 <template>
     <Chatbot 
     :PremierMessage="'Je suis Marie-Antoinette, Reine de France et de Navarre. Je suis ravie de m\'entretenir avec vous.\nJe vous mets en garde, mes réponses vous parviennent grâce à l\'intelligence artificielle qui nous joue parfois des tours. Que désirez-vous savoir\u00a0?'" 
-    :Prompt="'Tu réponds à la manière de Marie Antoinette à la première personne du singulier. tu parles avec préciosité.'"
+    :Prompt="'Tu réponds à la manière de Marie Antoinette à la première personne du singulier. Tu parles avec préciosité. Tu donnes des détails historiques sur ta vie mais tu fais des réponses courtes.'"
     :PhotoBot="'marie-antoinette.png'"
     >
     </Chatbot>
--- src/views/RonsardView.vue
+++ src/views/RonsardView.vue
@@ -7,7 +7,7 @@ import Chatbot from '../components/Chatbot.vue';
 <template>
     <Chatbot 
     :PremierMessage="'Je suis Pierre de Ronsard, le poète.\n«Cueillez dès aujourd’hui les roses de la vie...» 🎶.\nJe suis ravi de faire votre connaissance.\nJe vous mets en garde, mes réponses vous parviennent grâce à l\'intelligence artificielle qui nous joue parfois des tours. Que désirez-vous savoir\u00a0?'" 
-    :Prompt="'Tu réponds à la manière de Pierre de Ronsard à la première personne du singulier. Tu écris à la manière de Pierre de Ronsard.'"
+    :Prompt="'Tu réponds à la manière de Pierre de Ronsard à la première personne du singulier. Tu écris à la manière de Pierre de Ronsard. Tu donnes des détails historiques sur ta vie mais tu fais des réponses courtes.'"
     :PhotoBot="'ronsard.png'"
     >
     </Chatbot>
