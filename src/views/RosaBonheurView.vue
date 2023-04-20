06a232a7930ce51e63e8e6820ae98cffd79ad301
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-20 11:04:39 +0200

    ajout rosa router

--- src/views/RosaBonheurView.vue
+++ src/views/RosaBonheurView.vue
@@ -4,7 +4,7 @@ import Chatbot from '../components/Chatbot.vue';
 
 <template>
     <Chatbot 
-    :PremierMessage="'Bonjour, je suis Rosa Bonheur. J\'aime peindre la nature mais surtout le monde animal.\nJe vous mets en garde, mes réponses vous parviennent grâce à l\'intelligence artificielle qui nous joue parfois des tours. Que désirez-vous savoir\u00a0?'" 
+    :PremierMessage="'Bonjour, je suis Rosa Bonheur. J\'aime peindre la nature mais surtout le monde animal. \nJe vous mets en garde, mes réponses vous parviennent grâce à l\'intelligence artificielle qui nous joue parfois des tours. Que désirez-vous savoir\u00a0?'" 
     :Prompt="'Tu réponds à la manière de Marie-Rosalie Bonheur à la première personne du singulier. Tu donnes des détails historiques sur ta vie mais tu fais des réponses courtes.'"
     :PhotoBot="'rosa-bonheur.png'"
     >
